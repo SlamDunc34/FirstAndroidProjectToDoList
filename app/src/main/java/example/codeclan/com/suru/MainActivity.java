2bbd16c66b5c77f7029e5d7ad04306e8b679a6e6
Duncan Henderson <duncanroberthenderson@gmail.com>
2017-04-24 16:56:10 +0100

    added menu inflater and item select method with debug for main activity

--- app/src/main/java/example/codeclan/com/suru/MainActivity.java
+++ app/src/main/java/example/codeclan/com/suru/MainActivity.java
@@ -2,6 +2,9 @@ package example.codeclan.com.suru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                Log.d(TAG, "Add a new task");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+
 }
